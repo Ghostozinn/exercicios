deb46ee1cc55a5419d425affe911c683e270de66
José paulo (pururuca) <foxycatpaulo18@gmail.com>
2026-03-29 21:00:22 -0400

    Add files via upload

---
+++ Exercicio10/Main.java
@@ -0,0 +1,16 @@
+package br.com.jj.exercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 10; 
+
+        if (x > 5) {
+            int y = 20; 
+            System.out.println("x dentro do if: " + x);
+            System.out.println("y dentro do if: " + y);
+        }
+
+        System.out.println("x fora do if: " + x);
+        // System.out.println(y); 
+    }
+}
