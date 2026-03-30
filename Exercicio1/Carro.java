6864e2e8d5b52e7a920e5dfe377894c8b182a912
José paulo (pururuca) <foxycatpaulo18@gmail.com>
2026-03-29 20:58:30 -0400

    Update Carro.java

--- Exercicio1/Carro.java
+++ Exercicio1/Carro.java
@@ -1,8 +1,8 @@
 package br.com.jj.exercicios;
 
-// Exercício 1 - Classe e objeto [cite: 11]
+
 public class Carro {
-    // Atributos (Acesso direto para o Ex 1) [cite: 12]
+    
     public String marca;
     public String modelo;
     public int ano;
