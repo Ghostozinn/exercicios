00e76fe0c80329109a6e4f49c247ef79df6d5510
José paulo (pururuca) <foxycatpaulo18@gmail.com>
2026-03-29 21:11:46 -0400

    Add methods to display car info and calculate age

--- Exercicio2/Carro.java
+++ Exercicio2/Carro.java
@@ -5,12 +5,12 @@ public class Carro {
     public String modelo;
     public int ano;
 
-    // Exercício 2a - Método void: Apenas executa a ação de imprimir
+    
     public void exibir() {
         System.out.println("Carro: " + marca + " " + modelo + " | Ano: " + ano);
     }
 
-    // Exercício 2b - Método com retorno: Devolve um número inteiro (int)
+    
     public int idadeDoCarro() {
         return 2026 - ano;
     }
