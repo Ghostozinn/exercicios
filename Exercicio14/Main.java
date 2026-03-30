5f7bbf041cb84145b7009ab634901fe1c3971a2c
José paulo (pururuca) <foxycatpaulo18@gmail.com>
2026-03-29 21:08:29 -0400

    Add files via upload

---
+++ Exercicio14/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente("João Silva", "123.456.789-00");
+        Cliente c2 = new Cliente("Maria Oliveira");
+
+        c1.exibir();
+        c2.exibir();
+    }
+}
